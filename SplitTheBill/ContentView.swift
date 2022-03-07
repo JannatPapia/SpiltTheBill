0648d65d52fe60dcd630a09be97f26e685bdaf49
Jannatpapia <papiamcode@gmail.com>
2022-03-07 18:24:50 +0600

    Added some variable and function

--- SplitTheBill/ContentView.swift
+++ SplitTheBill/ContentView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //Add variable
+    @State private var totalCost = ""
+    @State private var people = 4
+    @State private var tipIndex = 2
+    let tipPercentages = [0, 5, 10, 15]
+    
+    // Create a function
+    func calculateTotal() -> Double {
+        let tip = Double(tipPercentages[tipIndex])
+        let orderTotal = Double(totalCost) ?? 0
+        let finalAmount = ((orderTotal / 100 * tip) + orderTotal)
+        return finalAmount / Double(people)
+    }
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
