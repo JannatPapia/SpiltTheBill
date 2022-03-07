6d1010413ac6c11b668d60a41c29391084334568
Jannatpapia <papiamcode@gmail.com>
2022-03-07 16:36:47 +0600

    project setup done

---
+++ SplitTheBill/SplitTheBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTheBillApp.swift
+//  SplitTheBill
+//
+//  Created by Jannatun Nahar Papia  on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitTheBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
